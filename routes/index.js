4859f6bdcab41ff7ce8618770cff40c0142b3e81
Paul Pierzchlewicz <PPierzc@users.noreply.github.com>
2017-11-29 08:10:18 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,3 @@
-/**
- * This file is where you define your application routes and controllers.
- *
- * Start by including the middleware you want to run for every request;
- * you can attach middleware to the pre('routes') and pre('render') events.
- *
- * For simplicity, the default setup for route controllers is for each to be
- * in its own file, and we import all the files in the /routes/views directory.
- *
- * Each of these files is a route controller, and is responsible for all the
- * processing that needs to happen for the route (e.g. loading data, handling
- * form submissions, rendering the view template, etc).
- *
- * Bind each route pattern your application should respond to in the function
- * that is exported from this module, following the examples below.
- *
- * See the Express application routing documentation for more information:
- * http://expressjs.com/api.html#app.VERB
- */
-
 var keystone = require('keystone');
 var middleware = require('./middleware');
 var importRoutes = keystone.importer(__dirname);
@@ -44,8 +24,5 @@ exports = module.exports = function (app) {
 	app.post('/api/addfriend/:id', routes.views.addfriend);
 	app.post('/api/removefriend/:id', routes.views.removefriend);
 	app.post('/api/newuser/', routes.views.newuser);
-
-	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
-	// app.get('/protected', middleware.requireUser, routes.views.protected);
 
 };
